446856308b47beec8917eaa25f8d384aa399e0e6
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-07-16 10:13:32 +0200

    Updates

--- pipelines/aggregate.py
+++ pipelines/aggregate.py
@@ -107,27 +107,47 @@ def sample(src, dst, aggregation_id, x, y, z, buffer, use_lerc=False):
     '''
     utils.run_command(command)
 
-def get_maxzoom_from_covering_tiles(covering_tiles):
-    items_with_source = covering_tiles[-1]
+def get_maxzoom_from_aggregation_item(aggregation_item):
+    items_with_source = aggregation_item[-1]
     source = items_with_source['source']
     collection_ids = utils.get_collection_ids(source)
     return utils.get_maxzoom(source, collection_ids[-1])
 
-def has_nodata(source, item):
-    filename = f'cogify-store/3857/{source}/{item["collection_id"]}/{item["z"]}-{item["x"]}-{item["y"]}.tiff'
-    da = rioxarray.open_rasterio(filename)
-    return da.rio.nodata in da.values[0]
+def can_copy_directly(aggregation_tile, aggregation_item):
+    if len(aggregation_item) != 1:
+        return False
+    cogify_items = aggregation_item[0]['items']
+    if len(cogify_items) != 1:
+        return False
+    cogify_item = cogify_items[0]
+    if cogify_item['x'] != aggregation_tile.x:
+        return False
+    if cogify_item['y'] != aggregation_tile.y:
+        return False
+    if cogify_item['z'] != aggregation_tile.z:
+        return False
+    return True
 
-def filter_covering_tiles(macrotile, covering_tiles):
-    result = []
-    for items_with_source in covering_tiles:
-        overlapping_items = []
-        for item in items_with_source['items']:
-            if utils.are_tiles_overlapping(macrotile, mercantile.Tile(x=item['x'], y=item['y'], z=item['z'])):
-                overlapping_items.append(item)
-        if len(overlapping_items) > 0:
-            result.append(items_with_source)
-    return result
+def copy_directly(aggregtion_tile, aggregation_item, aggregation_id):
+    source = aggregation_item[0]['source']
+    cogify_item = aggregation_item[0]['items'][0]
+    fetch_tiff(source, cogify_item['collection_id'], cogify_item['x'], cogify_item['y'], cogify_item['z'])
+    src = f'cogify-store/3857/{source}/{cogify_item["collection_id"]}/{cogify_item["z"]}-{cogify_item["x"]}-{cogify_item["y"]}.tiff'
+    dst = f'aggregation-store/{aggregation_id}/{aggregation_tile.z}-{aggregation_tile.x}-{aggregation_tile.y}.tiff'
+    shutil.copyfile(src, dst)
+
+def update_coverage(aggregation_id):
+    coverage = {
+        'type': 'FeatureCollection',
+        'features': []
+    }
+    for filepath in glob(f'aggregation-store/{aggregation_id}/*.tiff'):
+        _, __, aggregation_item_name = filepath.replace('.tiff', '').split('/')
+        z, x, y = [int(a) for a in aggregation_item_name.split('-')]
+        aggregation_tile = mercantile.Tile(x=x, y=y, z=z)
+        coverage['features'].append(mercantile.feature(aggregation_tile))
+    with open('tmp/coverage.geojson', 'w') as f:
+        json.dump(coverage, f, indent=2)
 
 # prepare local cogify store
 remote_cogify_store = f'{local_config.remote_cogify_store_path}/3857/'
@@ -139,42 +159,45 @@ utils.rsync(src=remote_cogify_store, dst=local_cogify_store, skip_tiffs=True)
 aggregation_ids = utils.get_aggregation_ids()
 aggregation_id = aggregation_ids[-1]
 
-z = local_config.macrotile_z
-paths = list(sorted(glob(f'aggregation-store/{aggregation_id}/{z}-*-*.json')))
+aggregation_item_paths = list(sorted(glob(f'aggregation-store/{aggregation_id}/*-*-*.json')))
 j = 0
 t0 = time.time()
-for path in paths:
-    print(f'working on file {j} / {len(paths)}. Total time so far {time.time() - t0} s.')
+for aggregation_item_path in aggregation_item_paths:
+    print(f'working on file {j} / {len(aggregation_item_paths)}. Total time so far {time.time() - t0} s.')
     tic = time.time()
     j += 1
 
-    _, __, item_name = path.replace('.json', '').split('/')
-    _, x, y = item_name.split('-')
+    _, __, aggregation_item_name = aggregation_item_path.replace('.json', '').split('/')
+    z, x, y = aggregation_item_name.split('-')
+    z = int(z)
     x = int(x)
     y = int(y)
 
-    macrotile_filename = f'aggregation-store/{aggregation_id}/{z}-{x}-{y}.tiff'
-    macrotile = mercantile.Tile(x=x, y=y, z=z)
+    aggregation_tile_filename = f'aggregation-store/{aggregation_id}/{z}-{x}-{y}.tiff'
+    aggregation_tile = mercantile.Tile(x=x, y=y, z=z)
 
-    # if os.path.isfile(macrotile_filename):
-    #     print(f'macrotile x={x} y={y} z={z} exists already...')
-    #     continue
-
-    if x != 2116 or y != 1450:
+    if os.path.isfile(aggregation_tile_filename):
+        print(f'aggregation tile x={x} y={y} z={z} exists already...')
         continue
+
+    # if x != 2131 or y != 1432:
+    #     continue
     # if x != 8520 or y != 5836:
     #     continue
 
-    covering_tiles = []
-    with open(path) as f:
-        covering_tiles = json.load(f)
+    aggregation_item = []
+    with open(aggregation_item_path) as f:
+        aggregation_item = json.load(f)
 
-    covering_tiles = filter_covering_tiles(macrotile, covering_tiles)
+    if can_copy_directly(aggregation_tile, aggregation_item):
+        print(f'can copy aggregation tile {aggregation_tile} directly...')
+        # copy_directly(aggregation_tile, aggregation_item, aggregation_id)
+        continue
 
-    zoom = get_maxzoom_from_covering_tiles(covering_tiles)
+    zoom = get_maxzoom_from_aggregation_item(aggregation_item)
     da_merged = None
 
-    for items_with_source in reversed(covering_tiles):
+    for items_with_source in reversed(aggregation_item):
         if da_merged is not None and da_merged.rio.nodata not in da_merged:
             break
 
@@ -184,18 +207,18 @@ for path in paths:
             fetch_tiff(source, item['collection_id'], item['x'], item['y'], item['z'])
         
         part_name = f'part-{source}'
-        make_vrt(source, items, aggregation_id, x, y, local_config.macrotile_z, part_name)
+        make_vrt(source, items, aggregation_id, x, y, z, part_name)
 
-        if len(covering_tiles) == 1:
+        if len(aggregation_item) == 1:
             break
 
         current_zoom = utils.get_maxzoom(source, items[0]['collection_id'])
         src = f'aggregation-store/{aggregation_id}/{z}-{x}-{y}-parts/{part_name}.vrt'
         dst = f'aggregation-store/{aggregation_id}/{z}-{x}-{y}-parts/{part_name}.tiff'
-        # if current_zoom == zoom:
-        #     sample(src, dst, aggregation_id, x, y, z, local_config.macrotile_buffer_m)
-        # else:
-        #     upsample(src, dst, aggregation_id, x, y, z, zoom, local_config.macrotile_buffer_m)
+        if current_zoom == zoom:
+            sample(src, dst, aggregation_id, x, y, z, local_config.macrotile_buffer_m)
+        else:
+            upsample(src, dst, aggregation_id, x, y, z, zoom, local_config.macrotile_buffer_m)
 
         da_current = rioxarray.open_rasterio(f'aggregation-store/{aggregation_id}/{z}-{x}-{y}-parts/{part_name}.tiff')
 
@@ -203,53 +226,59 @@ for path in paths:
             da_merged = da_current.copy()
             continue
         
+        t1 = time.time()
         binary_mask = (da_merged.values[0] != da_merged.rio.nodata).astype('int32')
-        print('binary_mask done...')
+        print(f'binary_mask done in {time.time() - t1} s...')
 
         bounds = mercantile.xy_bounds(mercantile.Tile(x=x, y=y, z=z))
         resolution = (bounds.right - bounds.left) / 2 ** (zoom - z) / 512
-        max_pixel_distance = int(0.75*local_config.macrotile_buffer_m / resolution)
+        max_pixel_distance = int(0.5 * local_config.macrotile_buffer_m / resolution)
         print('max_pixel_distance', max_pixel_distance)
-        reduced = ndimage.binary_erosion(binary_mask, iterations=max_pixel_distance)
-        print('binary erosion done...')
 
+        t1 = time.time()
+        reduced = ndimage.binary_erosion(binary_mask, iterations=max_pixel_distance)
+        print(f'binary erosion done in {time.time() - t1} s...')
+
+        # t1 = time.time()
         # eroded = ndimage.binary_erosion(reduced)
         # edges = reduced ^ eroded
-        # print('edges done...')
+        # print(f'edges done in {time.time() - t1} s...')
 
-
+        t1 = time.time()
         # distance_to_edge = ndimage.distance_transform_edt(1 - edges)
         # distance_to_edge = np.where(distance_to_edge > max_pixel_distance, max_pixel_distance, distance_to_edge)
         # distance_to_edge = np.where((1 - reduced), 0.0, distance_to_edge)
         # distance_to_edge = ndimage.uniform_filter(distance_to_edge, max_pixel_distance, mode='nearest')
         # distance_to_edge = np.where((1 - binary_mask), 0.0, distance_to_edge)
-        # print('distance_to_edge done...')
-
+        # print(f'distance_to_edge done in {time.time() - t1} s...')
         # alpha_mask = distance_to_edge / max_pixel_distance
 
-        alpha_mask = ndimage.uniform_filter(reduced.astype('float32'), int(0.75 * max_pixel_distance), mode='nearest')
+        t1 = time.time()
+
+        alpha_mask = ndimage.uniform_filter(reduced.astype('float32'), int(1.25 * max_pixel_distance), mode='nearest')
         alpha_mask = 3 * alpha_mask ** 2 - 2 * alpha_mask ** 3 # smoothstep with zero derivative at 0 and 1
         alpha_mask = np.where((1 - binary_mask), 0.0, alpha_mask)
-        print('alpha_mask done...')
+        print(f'alpha_mask done in {time.time() - t1} s...')
 
         # da_out = da_merged.copy()
         # da_out.values[0] = alpha_mask
         # da_out.rio.to_raster('alpha_mask2.tiff')
         # exit()
 
+        t1 = time.time()
         merged = da_current.values[0] * (1 - alpha_mask) + da_merged.values[0] * alpha_mask
         da_merged.values[0] = merged
-        print('merging done...')
+        print(f'merging done in {time.time() - t1} s...')
 
     input_filename = ''
-    if len(covering_tiles) > 1:
+    if len(aggregation_item) > 1:
         merged_filename = f'aggregation-store/{aggregation_id}/{z}-{x}-{y}-parts/merged.tiff'
         da_merged.rio.to_raster(merged_filename)
         input_filename = merged_filename
     else:
-        input_filename = f'aggregation-store/{aggregation_id}/{z}-{x}-{y}-parts/part-{covering_tiles[0]["source"]}.vrt'
-    macrotile_filename = f'aggregation-store/{aggregation_id}/{z}-{x}-{y}.tiff'
-    sample(input_filename, macrotile_filename, aggregation_id, x, y, z, 0, use_lerc=True)
-    # shutil.rmtree(f'aggregation-store/{aggregation_id}/{z}-{x}-{y}-parts/')
+        input_filename = f'aggregation-store/{aggregation_id}/{z}-{x}-{y}-parts/part-{aggregation_item[0]["source"]}.vrt'
+    sample(input_filename, aggregation_tile_filename, aggregation_id, x, y, z, 0, use_lerc=True)
+    shutil.rmtree(f'aggregation-store/{aggregation_id}/{z}-{x}-{y}-parts/')
 
-    print(f'macrotile completed in {time.time() - tic} s')
+    update_coverage(aggregation_id)
+    print(f'aggregation tile completed in {time.time() - tic} s')
