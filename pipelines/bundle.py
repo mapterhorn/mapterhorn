a048d8eff34d3c7138d043d3691c444b3c93cb0f
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-08-15 15:11:07 +0200

    Clustered write

--- pipelines/bundle.py
+++ pipelines/bundle.py
@@ -39,6 +39,15 @@ def get_parent_to_filepaths():
 
     return parent_to_filepath
 
+def read_full_archive(filepath):
+    tile_id_to_bytes = {}
+    with open(filepath , 'r+b') as f2:
+        reader = Reader(MmapSource(f2))
+        for tile_tuple, tile_bytes in all_tiles(reader.get_bytes):
+            tile_id = zxy_to_tileid(*tile_tuple)
+            tile_id_to_bytes[tile_id] = tile_bytes
+    return tile_id_to_bytes
+
 def create_archive(filepaths, out_filepath):
     with open(out_filepath, 'wb') as f1:
         writer = Writer(f1)
@@ -48,24 +57,45 @@ def create_archive(filepaths, out_filepath):
         min_lat = math.inf
         max_lon = -math.inf
         max_lat = -math.inf
-        for j, filepath in enumerate(filepaths):
-            filename = filepath.split('/')[-1]
-            print(f'{filename} {j + 1} / {len(filepaths)}')
-            z, x, y, _ = [int(a) for a in filename.replace('.pmtiles', '').split('-')]
-            
-            with open(filepath , 'r+b') as f2:
-                reader = Reader(MmapSource(f2))
-                for tile_tuple, tile_bytes in all_tiles(reader.get_bytes):
-                    tile_id = zxy_to_tileid(*tile_tuple)
-                    writer.write_tile(tile_id, tile_bytes)
 
-            max_z = max(max_z, z)
-            min_z = min(min_z, z)
+        tile_ids_and_filepaths = []
+
+        for filepath in filepaths:
+            filename = filepath.split('/')[-1]
+            z, x, y, child_z = [int(a) for a in filename.replace('.pmtiles', '').split('-')]
+            parent = mercantile.Tile(x=x, y=y, z=z)
+            tiles = []
+            if z == child_z:
+                tiles.append(parent)
+            else:
+                tiles += mercantile.children(parent, zoom=child_z)
+            for tile in tiles:
+                tile_id = zxy_to_tileid(tile.z, tile.x, tile.y)
+                tile_ids_and_filepaths.append((tile_id, filepath))
+        
+            max_z = max(max_z, child_z)
+            min_z = min(min_z, child_z)
             west, south, east, north = mercantile.bounds(x, y, z)
             min_lon = min(min_lon, west)
             min_lat = min(min_lat, south)
             max_lon = max(max_lon, east)
             max_lat = max(max_lat, north)
+
+        tile_ids_and_filepaths = sorted(tile_ids_and_filepaths)
+        
+        last_filepath = None
+        tile_id_to_bytes = None
+
+        j = 0
+        for tile_id, filepath in tile_ids_and_filepaths:
+            if filepath != last_filepath:
+                last_filepath = filepath
+                tile_id_to_bytes = read_full_archive(filepath)
+            writer.write_tile(tile_id, tile_id_to_bytes[tile_id])
+
+            j += 1
+            if j % 10000 == 0:
+                print(f'processed {j} / {len(tile_ids_and_filepaths)} tiles...')
 
         min_lon_e7 = int(min_lon * 1e7)
         min_lat_e7 = int(min_lat * 1e7)
