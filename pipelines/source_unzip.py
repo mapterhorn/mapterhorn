7a5e7a785d1f5ae74a129981435c1a5fd8b65d55
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-09-11 23:34:55 +0200

    Add source denrw: Nordrhein-Westfalen, Germany 1m

--- pipelines/source_unzip.py
+++ pipelines/source_unzip.py
@@ -27,43 +27,21 @@ def un7z(filepath, source):
         utils.run_command(f'rm "{filepath_to_remove}"', silent=SILENT)
 
 def translate_images(filepath, source, suffix):
+    # suffix = 'asc' or 'tif' u.s.w. (without dot)
     image_filepaths = glob(f'{filepath}-tmp/**/*.{suffix}', recursive=True)
     for image_filepath in image_filepaths:
         image_filename = image_filepath.split('/')[-1]
 
         filepath_in = image_filepath
         filepath_out = f'source-store/{source}/{image_filename}'
-        if filepath_in.endswith('.tif'):
-            pass
-        elif filepath_in.endswith('.TIF'):
-            filepath_out = filepath_out[:-3] + 'tif'
-        elif filepath_in.endswith('.xyz'):
-            filepath_out = filepath_out[:-3] + 'tif'
-        elif filepath_in.endswith('.asc'):
-            filepath_out = filepath_out[:-3] + 'tif'
+        suffix_length = len(suffix)
+        filepath_out = filepath_out[:-suffix_length] + 'tif'
 
-        # utils.run_command(f'mv "{image_filepath}" "source-store/{source}/{image_filename}"', silent=SILENT)
         utils.run_command(f'gdal_translate -of COG -co COMPRESS=LZW -co OVERVIEWS=NONE -co SPARSE_OK=YES -co BLOCKSIZE=512 -co BIGTIFF=YES "{filepath_in}" "{filepath_out}"', silent=False)
 
 def is_7z_head_file(filepath):
     return filepath.endswith('.7z') or filepath.endswith('.7z.001')
 
-# def to_cog(filepath):
-#     filepath_in = None
-#     filepath_out = None
-#     if filepath.endswith('.tif') or filepath.endswith('.TIF'):
-#         utils.run_command(f'mv {filepath} {filepath}.bak', silent=False)
-#         filepath_in = f'{filepath}.bak'
-#         filepath_out = filepath
-#     elif filepath.endswith('.xyz'):
-#         filepath_in = filepath
-#         filepath_out = filepath.replace('.xyz', '.tif')
-#     elif filepath.endswith('.asc'):
-#         filepath_in = filepath
-#         filepath_out = filepath.replace('.asc', '.tif')
-    
-#     utils.run_command(f'gdal_translate -of COG -co COMPRESS=LZW -co OVERVIEWS=NONE -co SPARSE_OK=YES -co BLOCKSIZE=512 -co BIGTIFF=YES "{filepath_in}" "{filepath_out}"', silent=False)
-#     utils.run_command(f'rm "{filepath_in}"', silent=False)
 
 def main():
     source = None
@@ -85,17 +63,8 @@ def main():
         translate_images(filepath, source, 'tif')
         translate_images(filepath, source, 'TIF')
         translate_images(filepath, source, 'asc')
+        translate_images(filepath, source, 'ASC')
         translate_images(filepath, source, 'xyz')
-
-        # input_filepaths = []
-        # input_filepaths += glob(f'source-store/{source}/*.tif')
-        # input_filepaths += glob(f'source-store/{source}/*.TIF')
-        # input_filepaths += glob(f'source-store/{source}/*.xyz')
-        # input_filepaths += glob(f'source-store/{source}/*.asc')
-
-        # argument_tuples = [(path,) for path in input_filepaths]
-        # with Pool() as pool:
-        #     pool.starmap(to_cog, argument_tuples)
         
         tmpdir = f'{filepath}-tmp'
         if os.path.isdir(tmpdir):
