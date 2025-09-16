ee4f7d4ba786832bb1f68dff2c88e31ea451f9eb
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-09-16 10:25:18 +0200

    Use lerc in source_to_cog

--- pipelines/source_to_cog.py
+++ pipelines/source_to_cog.py
@@ -20,7 +20,7 @@ def to_cog(filepath):
         filepath_in = filepath
         filepath_out = filepath.replace('.txt', '.tif')
     
-    utils.run_command(f'gdal_translate -of COG -co COMPRESS=LZW -co OVERVIEWS=NONE -co SPARSE_OK=YES -co BLOCKSIZE=512 -co BIGTIFF=YES "{filepath_in}" "{filepath_out}"', silent=False)
+    utils.run_command(f'gdal_translate -of COG -co BLOCKSIZE=512 -co OVERVIEWS=NONE -co SPARSE_OK=YES -co BIGTIFF=YES -co COMPRESS=LERC -co MAX_Z_ERROR=0.001 "{filepath_in}" "{filepath_out}"', silent=False)
     utils.run_command(f'rm "{filepath_in}"', silent=False)
 
 def main():
