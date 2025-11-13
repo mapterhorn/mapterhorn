ce424f830cb0ef37024a9dbebe98da6d5b5c508b
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-11-13 11:02:02 +0100

    Add source tarball script

---
+++ pipelines/source_create_tarball.py
@@ -0,0 +1,28 @@
+from glob import glob
+import sys
+import tarfile
+
+def main():
+    source = None
+    if len(sys.argv) == 2:
+        source = sys.argv[1]
+        print(f'creating tarball for {source}...')
+    else:
+        print('Not enough arguments. Usage: source_create_tarball.py {{source}}')
+        exit()
+    
+    with tarfile.open(f'tar-store/{source}.tar', 'w') as tar:
+        tar.add(f'../source-catalog/{source}/LICENSE.pdf', 'LICENSE.pdf')
+        tar.add(f'../source-catalog/{source}/metadata.json', 'metadata.json')
+        tar.add(f'source-store/{source}/bounds.csv', 'bounds.csv')
+        tar.add(f'polygon-store/{source}.gpkg', 'coverage.gpkg')
+        filepaths = glob(f'source-store/{source}/*.tif')
+        for j, filepath in enumerate(filepaths, 1):
+            if j % 1000 == 0:
+                print(f'{j:_} / {len(filepaths):_}')
+            filename = filepath.split('/')[-1]
+            tar.add(filepath, f'files/{filename}')
+
+if __name__ == '__main__':
+    main()
+
