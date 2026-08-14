be60c77fab2b36cad74d2a1c84d14d78c5ac2f5a
Just van den Broecke <just@justobjects.nl>
2026-08-14 16:17:09 +0200

    #247 further refine filelist fetching from OGC API Features - make .py self-contained

--- source-catalog/nl50cm/get-file-list.sh
+++ source-catalog/nl50cm/get-file-list.sh
@@ -3,12 +3,4 @@
 # In 2026 AHN5 and AHN6 each are incomplete for NL, but together cover whole NL.
 # Revisit in 2027!
 
-AHN5_ITEMS="https://api.ellipsis-drive.com/v3/ogc/features/65945b69-81df-4270-97f0-f029033154c1/collections/01170035-93d3-4a38-b04c-8e7be7a7ca78/items"
-AHN6_ITEMS="https://api.ellipsis-drive.com/v3/ogc/features/0820faae-5240-499b-8486-cf406433cf71/collections/6aec07f5-f7eb-4f51-b6f7-aee45e5767bd/items"
-
-python getfilelist.py ${AHN5_ITEMS} "Maaiveldmodel (DTM) ½m" ahn5-files.txt
-python getfilelist.py ${AHN6_ITEMS} "Maaiveldmodel (DTM) ½m" ahn6-files.txt
-
-echo "merging: AHN5-DTM-50cm: $(wc -l ahn5-files.txt) files + AHN6-DTM-50cm: $(wc -l ahn6-files.txt) files"
-cat ahn5-files.txt ahn6-files.txt | sort -u | sed '/^$/d' > file_list.txt
-/bin/rm ahn5-files.txt ahn6-files.txt
+python getfilelist.py dtm_50cm file_list.txt
